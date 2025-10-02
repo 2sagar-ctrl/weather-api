859be0faf0dc60e3ab57a77c6e1f9448cd8abac8
2Sagar-ctrl <sagardas.path@gmail.com>
2025-10-02 17:02:00 +0530

    aqi added

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const currentWeatherDescEl = document.getElementById("current-weather-desc");
   const currentWeatherIconEl = document.getElementById("current-weather-icon");
   const forecastContainer = document.getElementById("forecast-container");
+  const sunriseTimeEl = document.getElementById("sunrise-time");
+  const sunsetTimeElr = document.getElementById("sunset-time");
+  const humidityEl = document.getElementById("humidity");
+  const windSpeedEl = document.getElementById("wind-speed");
+  const feelsLikeEl = document.getElementById("feels-like");
+  const pressureEl = document.getElementById("pressure");
+  const visiblityEl = document.getElementById("visiblity");
+  const airQualityEl = document.getElementById("air-quality");
+  const healthRecommendationsEl = document.getElementById(
+    "health-recommendations"
+  );
 
   const backgroundImageDay = {
     Clear:
@@ -81,16 +92,17 @@ document.addEventListener("DOMContentLoaded", () => {
           fetch(forecastUrl),
           fetch(aqiUrl),
         ]);
-if([weatherResponse, forecastResponse, aqiResponse].some(res => !res.ok)){
-     throw new Error ("Failed to fetch all weather data. Please check your API key and network connection.")
-
-}
+      if (
+        [weatherResponse, forecastResponse, aqiResponse].some((res) => !res.ok)
+      ) {
+        throw new Error(
+          "Failed to fetch all weather data. Please check your API key and network connection."
+        );
+      }
 
       const weatherData = await weatherResponse.json();
-        const forecastData = await forecastResponse.json();
-          const aqiData = await aqiResponse.json();
-
-
+      const forecastData = await forecastResponse.json();
+      const aqiData = await aqiResponse.json();
     } catch (error) {
       console.error("Weather data fetch error:", error);
       showError(error.message);
@@ -98,39 +110,146 @@ if([weatherResponse, forecastResponse, aqiResponse].some(res => !res.ok)){
       hideLoading();
     }
   };
- const updateUI = (weather,forecast,aqi)=>{
+  const updateUI = (weather, forecast, aqi) => {
     let weatherConditionForBg = weather.weather[0].main;
-    if(weatherConditionForBg === "Clouds" && weather.clouds.all<20){
-        weatherConditionForBg ="Clear";
+    if (weatherConditionForBg === "Clouds" && weather.clouds.all < 20) {
+      weatherConditionForBg = "Clear";
     }
     updateClock(weather.timezone);
-    clockInterval = setInterval(()=> updateClock(weather.timezone),1000);
+    clockInterval = setInterval(() => updateClock(weather.timezone), 1000);
 
     const currentTimeUTC = weather.dt;
     const sunriseUTC = weather.sys.sunrise;
     const sunsetUTC = weather.sys.sunset;
-    const isNight = (currentTimeUTC< sunriseUTC || currentTimeUTC> sunsetUTC);
+    const isNight = currentTimeUTC < sunriseUTC || currentTimeUTC > sunsetUTC;
 
-    const backgroundSet = isNight? backgroundImageNight : backgroundImageDay;
-    document.body.style,backgroundImage = `url('${backgroundSet[weatherConditionForBg]|| backgroundSet.Default}')`;
+    const backgroundSet = isNight ? backgroundImageNight : backgroundImageDay;
+    document.body.style,
+      (backgroundImage = `url('${
+        backgroundSet[weatherConditionForBg] || backgroundSet.Default
+      }')`);
 
-currentWeatherIconEl.src =`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`;
-cityNameEl.textContent= `${weather.name},${weather.sys.country}`;
-const localDate = new Data ((weather.dt+weather.timezone) *1000);
-currentDateEl.textContent = localDate.toLocalDataString("en-US",{weeday:"long",month:"long",day:"numeric",timezone:"UTC"});
-currentTempEl.textContent= `${Math.round(weather.main.temp)}°`;
-currentWeatherDescEl.textContent =weather.weather[0].description;
+    currentWeatherIconEl.src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`;
+    cityNameEl.textContent = `${weather.name},${weather.sys.country}`;
+    const localDate = new Data((weather.dt + weather.timezone) * 1000);
+    currentDateEl.textContent = localDate.toLocalDataString("en-US", {
+      weeday: "long",
+      month: "long",
+      day: "numeric",
+      timezone: "UTC",
+    });
+    currentTempEl.textContent = `${Math.round(weather.main.temp)}°`;
+    currentWeatherDescEl.textContent = weather.weather[0].description;
 
-const formatTime = (timestamp) => new Data(timestamp * 1000).toLocalTimeString
-("en-US",{hour:"2-digit",minute:"2-digit",hour12:true,timezone:"UTC"});
- }
+    const formatTime = (timestamp) =>
+      new Data(timestamp * 1000).toLocalTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+        timezone: "UTC",
+      });
+    sunriseTimeEl.textContent = formatTime(
+      weather.sys.sunrise + weather.timezone
+    );
+    sunsetTimeEl.textContent = formatTime(
+      weather.sys.sunset + weather.timezone
+    );
+    humidityEl.textContent = `${weather.main.humidity}%`;
+    windSpeedEl.textContent = `${(weather.wind.speed * 3.6).toFixed(1)}hm/hr`;
+    feelsLikeEl.textContent = `${Math.round(weather.main.feels_like)}°`;
+    pressureEl.textContent = `${weather.main.pressure}hpa`;
+    visiblityEl.textContent = `${(weather.visiblity / 1000).toFixed(1)}km`;
+    const aqiValue = aqi.list[0].main.aqi;
+    const aqiInfo = getAqiInfo(aqiValue);
+    airQualityEl = textContent = aqiInfo.text;
+    airQualityEl.className = `font-bold px-3 py-1 rounded-full text-sm ${aqiInfo.color}`;
+    healthRecommendationsEl.innerHTML = `<p class="text-gray-200 text-sm">${aqiInfo.recommendation}</p>`;
 
- const updateClock = (timezoneoffset)=>{
+const dailyForecasts = processForecast(forecast.list);
+
+
+  };
+
+  const getAqiInfo = (aqi) => {
+    switch (aqi) {
+      case 1:
+        return {
+          text: "Good",
+          color: "bg-green-500 text-white",
+          recommendation:
+            "Air quality is great. It is a perfect day to be active outside.",
+        };
+
+      case 2:
+        return {
+          text: "Fair",
+          color: "bg-yellow-500 text-black",
+          recommendation:
+            "Air quality is acceptable. Unusually sensitive people should consider reducing prolonged or heavy exertion.",
+        };
+
+      case 3:
+        return {
+          text: "Moderate",
+          color: "bg-orange-500 text-white",
+          recommendation:
+            "Sensitive groups may experience health effects. The general public is less likely to be affected.",
+        };
+
+      case 4:
+        return {
+          text: "Poor",
+          color: "bg-red-500 text-white",
+          recommendation:
+            "Everyone may experience health effects. Members of sensitive groups may experince more health effects.",
+        };
+
+      case 5:
+        return {
+          text: "Very Poor",
+          color: "bg-purple-700 text-white",
+          recommendation:
+            "Health alert:The risk of health effects is increased for everyone. Avoid outdoor activities .",
+        };
+
+      default:
+        return {
+          text: "Unknown",
+          color: "bg-gray-500 text-white",
+          recommendation: "Air quality data is not available at the moment.",
+        };
+    }
+  };
+const processForecast=(forecastList)=>{
+const dailyData ={};
+forecastList.forEach(entry => {
+    const data = entry.dt_txt.split(' ')[0];
+    if(!dailyData[date]){
+        dailyData[date]={temps_max:[],temps_min: [], icons:{},entry:null}
+    }
+    dailyData[date].temps_max.push(entry.main.temps_max);
+     dailyData[date].temps_min.push(entry.main.temps_min);
+     const icon = entry.weather[0].icon;
+     dailyData[date].icons[icon]=(dailydata[date].icons[icon]||0);
+     if(!dailyData[date].entry || entry.dt_txt.includes("12:00:00")){
+        dailyData[date].entry=entry;
+     }
+});
+
+
+
+}
+  const updateClock = (timezoneoffset) => {
     const now = new Data();
-    const utc = now.getTime()+(now.getTimezoneoffset()*60000);
-    const localTime = new Data (utc+(timezoneoffset * 1000));
-    currentTimeEl.textContent = localTime.toLocalTimeString("en-US",{hour:"2-digit",minute:"2-digit",second:"2-digit",hour12:true})
- }
+    const utc = now.getTime() + now.getTimezoneoffset() * 60000;
+    const localTime = new Data(utc + timezoneoffset * 1000);
+    currentTimeEl.textContent = localTime.toLocalTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+  };
   const showLoading = () => {
     loadingOverlay.classList.remove("hidden");
     loadingOverlay.classList.add("flex");
